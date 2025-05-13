93ad41521200bd799aeb4a9310da34aba5498e00
Danuka <danukaandhimasha@gmail.com>
2025-05-13 11:25:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "qMRz3CQB#lXJEQO3vMB6dpNXdzg7doFY1ySEJmffXpRZJkAfDXig",
+  SESSION_ID: process.env.SESSION_ID || "",
   MONGODB: process.env.MONGODB || "mongodb://mongo:uMASvQLlPvAMLMOnmMiUDpKPnIAgeZRU@yamanote.proxy.rlwy.net:16142",
   OWNER_NUM: process.env.OWNER_NUM || "94776121326",
 };
